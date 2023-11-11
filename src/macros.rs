1b2046f6b6b112f77ff9e1b9b70df0e75075d8fb
hinto.janai <hinto.janai@protonmail.com>
2023-11-10 20:32:22 -0500

    add log macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,6 @@
 // Global macros for internal sansan usage.
 
+//---------------------------------------------------------------------------------------------------- Channels
 // Receive a channel message, unwrap.
 macro_rules! recv {
     ($channel:expr) => {
@@ -10,8 +11,52 @@ pub(crate) use recv;
 
 // Send a channel message, unwrap.
 macro_rules! send {
-    ($channel:expr, $($msg:tt)*) => {
-        $channel.send($($msg)*).unwrap()
+    ($channel:expr, $($msg:tt)+) => {
+        $channel.send($($msg)+).unwrap()
     }
 }
 pub(crate) use send;
+
+//---------------------------------------------------------------------------------------------------- Logging
+// Logs with `log` but only if in debug
+// mode or if the log feature is enabled.
+
+macro_rules! error2 {
+    ($($arg:tt)+) => {
+        #[cfg(any(feature = "log"), (debug_assertions))]
+        ::log::error!($($arg)+);
+    };
+}
+pub(crate) use error2;
+
+macro_rules! warn2 {
+    ($($arg:tt)+) => {
+        #[cfg(any(feature = "log"), (debug_assertions))]
+        ::log::warn!($($arg)+);
+    };
+}
+pub(crate) use warn2;
+
+macro_rules! info2 {
+    ($($arg:tt)+) => {
+        #[cfg(any(feature = "log"), (debug_assertions))]
+        ::log::info!($($arg)+);
+    };
+}
+pub(crate) use info2;
+
+macro_rules! debug2 {
+    ($($arg:tt)+) => {
+        #[cfg(any(feature = "log"), (debug_assertions))]
+        ::log::debug!($($arg)+);
+    };
+}
+pub(crate) use debug2;
+
+macro_rules! trace2 {
+    ($($arg:tt)+) => {
+        #[cfg(any(feature = "log"), (debug_assertions))]
+        ::log::trace!($($arg)+);
+    };
+}
+pub(crate) use trace2;
