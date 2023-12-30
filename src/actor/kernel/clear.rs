3bce4c76c8e734190448f41990a9273b51bea828
hinto.janai <hinto.janai@protonmail.com>
2023-12-30 09:16:12 -0500

    kernel: add `clear()` test

--- src/actor/kernel/clear.rs
+++ src/actor/kernel/clear.rs
@@ -11,17 +11,15 @@ impl<Data: ValidData> Kernel<Data> {
 	pub(super) fn clear(&mut self, clear: Clear) {
 		match clear {
 			Clear::Queue => if self.queue_empty() { return },
-			Clear::Source => if !self.source_is_some() { return },
+			Clear::Current => if !self.source_is_some() { return },
 		}
 
 		self.w.add_commit_push(|w, _| {
 			match clear {
 				Clear::Queue => {
-					assert!(!w.queue.is_empty());
 					w.queue.clear();
 				},
-				Clear::Source => {
-					assert!(w.current.is_some());
+				Clear::Current => {
 					w.current = None;
 				},
 			}
@@ -32,4 +30,65 @@ impl<Data: ValidData> Kernel<Data> {
 //---------------------------------------------------------------------------------------------------- Tests
 #[cfg(test)]
 mod tests {
+	use super::*;
+	use crate::{
+		state::{AudioState,Current},
+		engine::Engine,
+		signal::{repeat::Repeat,volume::Volume,add::AddMany},
+	};
+	use pretty_assertions::assert_eq;
+
+	#[test]
+	fn clear() {
+		let (mut engine, _) = crate::tests::init_test();
+		let reader = engine.reader();
+		assert!(reader.get().queue.is_empty());
+
+		// Add sources to the queue.
+		let mut audio_state = AudioState::DEFAULT;
+		for i in 0..10 {
+			let source = crate::tests::source(i);
+			audio_state.queue.push_back(source);
+		}
+		// Set `Current`
+		audio_state.current = Some(Current {
+			source: audio_state.queue[4].clone(),
+			index: 4,
+			elapsed: 123.123,
+		});
+
+		engine.restore(audio_state.clone());
+		while *reader.get() != audio_state {
+			std::thread::sleep(std::time::Duration::from_millis(10));
+		}
+
+		assert_eq!(reader.get().queue.len(), 10);
+		assert_eq!(reader.get().current.as_ref().unwrap().index, 4);
+
+		//---------------------------------- Clear `Current`.
+		engine.clear(Clear::Current);
+		while reader.get().current.is_some() {
+			std::thread::sleep(std::time::Duration::from_millis(10));
+		}
+		assert_eq!(reader.get().current.is_none(), true);
+
+		//---------------------------------- Clear queue.
+		engine.clear(Clear::Queue);
+		while !reader.get().queue.is_empty() {
+			std::thread::sleep(std::time::Duration::from_millis(10));
+		}
+		assert_eq!(reader.get().queue.is_empty(), true);
+
+		//---------------------------------- Clear already empty `Current`.
+		let audio_state = reader.get();
+
+		engine.clear(Clear::Current);
+		std::thread::sleep(std::time::Duration::from_secs(1));
+		assert_eq!(reader.get(), audio_state);
+
+		//---------------------------------- Clear already empty queue.
+		engine.clear(Clear::Queue);
+		std::thread::sleep(std::time::Duration::from_secs(1));
+		assert_eq!(reader.get(), audio_state);
+	}
 }
