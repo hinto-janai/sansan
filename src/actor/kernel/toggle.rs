394d383af8162046445113d9c369d73e0eff41a0
hinto.janai <hinto.janai@protonmail.com>
2024-01-04 20:24:48 -0500

    kernel: add `toggle()` test

--- src/actor/kernel/toggle.rs
+++ src/actor/kernel/toggle.rs
@@ -14,10 +14,7 @@ impl<Data: ValidData> Kernel<Data> {
 		&mut self,
 		to_engine: &Sender<AudioStateSnapshot<Data>>,
 	) {
-		// INVARIANT:
-		// Both `pause()` and `play()` must:
-		// - `add_commit_push()`
-		// - `try_send!(to_engine, self.audio_state_snapshot())`
+		// INVARIANT: Both `pause()` and `play()` handle the details/channel/etc.
 		if self.playing() {
 			self.pause(to_engine);
 		} else {
@@ -29,4 +26,53 @@ impl<Data: ValidData> Kernel<Data> {
 //---------------------------------------------------------------------------------------------------- Tests
 #[cfg(test)]
 mod tests {
+	use super::*;
+	use crate::signal::SetIndex;
+	use crate::signal::add::{AddMany,InsertMethod};
+	use crate::state::{AudioState,Current};
+	use pretty_assertions::assert_eq;
+	use std::thread::sleep;
+	use std::time::Duration;
+
+	#[test]
+	fn toggle() {
+		let mut engine = crate::tests::init();
+		let reader = engine.reader();
+		let audio_state = reader.get();
+		assert_eq!(audio_state.queue.len(), 0);
+		assert_eq!(audio_state.playing, false);
+
+		//---------------------------------- Our baseline audio state
+		let audio_state = {
+			let mut audio_state = AudioState::DEFAULT;
+
+			for i in 0..10 {
+				let source = crate::tests::source(i);
+				audio_state.queue.push_back(source);
+			}
+
+			audio_state.current = None;
+			audio_state.playing = false;
+
+			audio_state
+		};
+		let resp = engine.restore(audio_state);
+		assert_eq!(resp.current.as_ref(), None);
+		assert_eq!(resp.playing, false);
+
+		//---------------------------------- No `Current`, early return
+		let resp = engine.toggle();
+		assert_eq!(resp.playing, false);
+
+		//---------------------------------- Set `Current`
+		let resp = engine.set_index(SetIndex { index: 0, play: None }).unwrap();
+		assert_eq!(resp.current.as_ref().unwrap().index, 0);
+		assert_eq!(resp.playing, false);
+
+		//---------------------------------- Toggle
+		let resp = engine.toggle();
+		assert_eq!(resp.playing, true);
+		let resp = engine.toggle();
+		assert_eq!(resp.playing, false);
+	}
 }
