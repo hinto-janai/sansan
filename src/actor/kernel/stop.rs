b5278c43b2aab81308166bcdd9430af3479293a5
hinto.janai <hinto.janai@protonmail.com>
2024-01-04 20:19:48 -0500

    kernel: add `stop()` test

--- src/actor/kernel/stop.rs
+++ src/actor/kernel/stop.rs
@@ -15,19 +15,16 @@ impl<Data: ValidData> Kernel<Data> {
 		&mut self,
 		to_engine: &Sender<AudioStateSnapshot<Data>>,
 	) {
-		if !self.current_is_some() || self.queue_empty() {
+		if self.queue_empty() {
 			try_send!(to_engine, self.audio_state_snapshot());
 			return;
 		}
 
-		self.w.add_commit(|w, _| {
-			assert!(w.current.is_some() || !w.queue.is_empty());
+		self.w.add_commit_push(|w, _| {
 			w.queue.clear();
 			w.current = None;
+			w.playing = false;
 		});
-		// The queue is empty, no need to re-apply,
-		// just clone the empty state.
-		self.w.push_clone();
 
 		try_send!(to_engine, self.audio_state_snapshot());
 	}
@@ -36,4 +33,46 @@ impl<Data: ValidData> Kernel<Data> {
 //---------------------------------------------------------------------------------------------------- Tests
 #[cfg(test)]
 mod tests {
+	use super::*;
+	use crate::state::{AudioState,Current};
+	use pretty_assertions::assert_eq;
+
+	#[test]
+	fn stop() {
+		let mut engine = crate::tests::init();
+		let audio_state = engine.reader().get();
+		assert_eq!(audio_state.queue.len(), 0);
+
+		//---------------------------------- Empty queue
+		let resp = engine.stop();
+		assert_eq!(resp, audio_state); // didn't change
+
+		//---------------------------------- Our baseline audio state
+		let audio_state = {
+			let mut audio_state = AudioState::DEFAULT;
+
+			for i in 0..10 {
+				let source = crate::tests::source(i);
+				audio_state.queue.push_back(source);
+			}
+
+			audio_state.current = Some(Current {
+				source: audio_state.queue[4].clone(),
+				index: 4,
+				elapsed: 0.0,
+			});
+			audio_state.playing = true;
+
+			audio_state
+		};
+		let resp = engine.restore(audio_state);
+		assert_eq!(resp.current.as_ref().unwrap().index, 4);
+		assert_eq!(resp.playing, true);
+
+		//---------------------------------- Clear queue, clear current.
+		let resp = engine.stop();
+		assert_eq!(resp.current.as_ref(), None);
+		assert_eq!(resp.queue.len(), 0);
+		assert_eq!(resp.playing, false);
+	}
 }
