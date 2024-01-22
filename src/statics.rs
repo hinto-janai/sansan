c4df9faf890b35c91b148fd7ecb6eb9554d5a21c
hinto.janai <hinto.janai@protonmail.com>
2024-01-22 17:57:18 -0500

    add `src/statics.rs`

---
+++ src/statics.rs
@@ -0,0 +1,16 @@
+//! Static data used throughout `sansan`.
+
+//---------------------------------------------------------------------------------------------------- Use
+use std::sync::{
+	OnceLock,Arc,
+};
+
+//----------------------------------------------------------------------------------------------------
+static EMPTY_ARC_STR_ONCE:  OnceLock<Arc<str>> = OnceLock::new();
+
+#[inline]
+#[allow(non_snake_case)]
+/// Returns a clone to a static reference to  `Arc("")`.
+pub(crate) fn EMPTY_ARC_STR() -> Arc<str> {
+	Arc::clone(EMPTY_ARC_STR_ONCE.get_or_init(|| Arc::from("")))
+}
